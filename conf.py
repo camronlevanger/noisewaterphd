d31c5f6300a0bad392d2b3bc1efa095b2b453e23
Camron Levanger <cam@needle.com>
2013-03-04 02:28:29 -0700

    google analytics fix

--- conf.py
+++ conf.py
@@ -303,16 +303,7 @@ COMMENTS_IN_GALLERIES = True
 
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
-ANALYTICS = "<script type=\"text/javascript\">var _gaq = _gaq || [];\
-            _gaq.push(['_setAccount', 'UA-38999144-1']);\
-            _gaq.push(['_trackPageview']);\
-            (function() {var ga = document.createElement('script'); \
-                ga.type = 'text/javascript'; \
-                ga.async = true;\
-                ga.src = ('https:' == document.location.protocol ? \
-                    'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\
-                var s = document.getElementsByTagName('script')[0];\
-                 s.parentNode.insertBefore(ga, s);})();</script>"
+ANALYTICS = "<script type=\"text/javascript\">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-38999144-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script');ga.type = 'text/javascript';ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga, s);})();</script>"
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
